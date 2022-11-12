60713867f67db9d9b42b4030541754b64c06edcb
lifay <lifay@LAPTOP-MMOMDL70>
2022-11-12 23:21:37 +0800

    添加项目文件。

---
+++ WMSPlugins/MenuList.cs
@@ -0,0 +1,17 @@
+﻿namespace WMSPlugins
+{
+    public class MenuList
+    {
+        public int Id { get; set; }
+        public string? iconUrl { get; set; }
+        public string title { get; set; }
+        public string? href { get; set; }
+        public string bakground { get; set; }
+        public string fontColor { get; set; }
+        public string Sidebarbackground { get; set; }
+
+
+        public List<MenuList>? Items { get; set; }
+    }
+}
+
